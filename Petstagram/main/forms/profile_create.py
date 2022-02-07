185087714a7eadb984ec183d8b4c9c19d632a378
Pavel Petkov <elias5247698@abv.bg>
2022-02-07 22:29:20 +0200

    making models [finished]

--- Petstagram/main/forms/profile_create.py
+++ Petstagram/main/forms/profile_create.py
@@ -1,25 +1,25 @@
 from django import forms
-
-
-# <form>
-
-#                 <button class="btn btn-primary mt-2" type="submit">Create</button>
-#             </form>
 from Petstagram.main.models.profile import ProfileModel
 from Petstagram.main.validators.name_validator import NameValidator
 
 
 class CreateProfileForm(forms.ModelForm):
+    FIRST_NAME_MIN_LENGTH = 2
+    SECOND_NAME_MIN_LENGTH = 2
+    FIRST_NAME_MAX_LENGTH = 30
+    SECOND_NAME_MAX_LENGTH = 30
     first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={
         "type": "text", "class": "form-control", "name": "first_name", "maxlength": 30, "required": True,
         "placeholder": "Enter first name", "id": "first_name"
     }), validators=[
-
+        NameValidator(FIRST_NAME_MIN_LENGTH, FIRST_NAME_MAX_LENGTH, "first name")
     ])
     last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={
         "type": "text", "class": "form-control", "name": "second_name", "maxlength": 30, "required": True,
         "placeholder": "Enter second name", "id": "id_last_name"
-    }))
+    }), validators=[
+        NameValidator(SECOND_NAME_MIN_LENGTH, SECOND_NAME_MAX_LENGTH, "second name")
+    ])
     profile_picture = forms.URLField(label="Link to Profile Image", max_length=30, widget=forms.URLInput(attrs={
         "type": "url", "class": "form-control", "name": "profile_image", "maxlength": 30, "required": True,
         "placeholder": "Enter URL", "id": "id_image"
--- Petstagram/main/models/pet_image.py
+++ Petstagram/main/models/pet_image.py
@@ -1,14 +1,19 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 from Petstagram.main.validators.image_size_validator import ValidateImage
-from Petstagram.main.models.pet import PetModel
 
 
 class PetImage(models.Model):
+    NAME_MAX_LENGTH = 30
     photo = models.FileField(validators=[
         ValidateImage(5)
     ])
+    tagged_pets = ArrayField(
+        models.TextField(models.CharField(max_length=NAME_MAX_LENGTH)
+                         ))
 
-    tagged_pets = models.TextField(
-        choices=PetModel.objects.all()  # will change this when stackoverflow is available
-    )
+    description = models.TextField()
+
+    creation_date = models.DateTimeField()
+    likes = models.IntegerField()
